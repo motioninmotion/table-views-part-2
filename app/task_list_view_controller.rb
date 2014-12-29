7300a9656cf16476006e20f13a15bd4121583227
Jack Dean Watson-Hamblin <info@fluffyjack.com>
2014-12-29 23:06:41 +1100

    Ep complete - brought back the task list view controller

---
+++ app/task_list_view_controller.rb
@@ -0,0 +1,19 @@
+class TaskListViewController < UITableViewController
+  TASK_CELL_ID = "TaskCell"
+
+  def loadView
+    super
+    tableView.registerClass(UITableViewCell, forCellReuseIdentifier: TASK_CELL_ID)
+    self.title = "Tasks"
+  end
+
+  def tableView(tableView, numberOfRowsInSection: section)
+    Tasks.all.count
+  end
+
+  def tableView(tableView, cellForRowAtIndexPath: indexPath)
+    cell = tableView.dequeueReusableCellWithIdentifier(TASK_CELL_ID, forIndexPath: indexPath)
+    cell.textLabel.text = Tasks.all[indexPath.row]
+    cell
+  end
+end
