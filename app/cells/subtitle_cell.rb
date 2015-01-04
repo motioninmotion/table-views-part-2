6679497160adb613c7f3c04f01a0dcb9ac72e021
Jack Dean Watson-Hamblin <info@fluffyjack.com>
2015-01-04 21:39:37 +1100

    Table views part 2 - finished episode by implementing the 4 cell subclasses and changing the task list view controller to use all 4.

---
+++ app/cells/subtitle_cell.rb
@@ -0,0 +1,7 @@
+class SubtitleCell < UITableViewCell
+  ID = "SubtitleCell"
+
+  def initWithStyle(style, reuseIdentifier: reuseIdentifier)
+    super(UITableViewCellStyleSubtitle, reuseIdentifier: reuseIdentifier)
+  end
+end
---
+++ app/cells/value_two_cell.rb
@@ -0,0 +1,7 @@
+class ValueTwoCell < UITableViewCell
+  ID = "ValueTwoCell"
+
+  def initWithStyle(style, reuseIdentifier: reuseIdentifier)
+    super(UITableViewCellStyleValue2, reuseIdentifier: reuseIdentifier)
+  end
+end
